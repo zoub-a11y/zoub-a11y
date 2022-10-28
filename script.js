37f0c6785164c434fa045f37e2d9504b53675ed7
Zoubeir Haffez <71153127+zoub-a11y@users.noreply.github.com>
2022-10-28 16:25:43 +0200

    Add files via upload

---
+++ script.js
@@ -0,0 +1,14 @@
+window.addEventListener('scroll', e => {
+    var btn = document.getElementById('btn');
+    if (window.scrollY > 200) { 
+        btn.classList.add('visible');
+    } else {
+        btn.classList.remove('visible');
+    }
+});
+function scrollhaut() {
+    window.scrollTo({
+        top: 0,
+        behavior: 'smooth'
+    });
+}
