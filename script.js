4cd4a778e6ab5ac7b5e75ef72642d2ff0dbd079b
Alexandre Achain (Weizu) <alexandre.achain@gmail.com>
2022-12-19 15:08:34 +0100

    Mise à jour de l'entête du linktree

--- script.js
+++ script.js
@@ -1,14 +1,14 @@
-window.addEventListener('scroll', e => {
-    var btn = document.getElementById('btn');
-    if (window.scrollY > 200) { 
-        btn.classList.add('visible');
-    } else {
-        btn.classList.remove('visible');
-    }
+window.addEventListener("scroll", (e) => {
+  var btn = document.getElementById("btn");
+  if (window.scrollY > 200) {
+    btn.classList.add("visible");
+  } else {
+    btn.classList.remove("visible");
+  }
 });
 function scrollhaut() {
-    window.scrollTo({
-        top: 0,
-        behavior: 'smooth'
-    });
+  window.scrollTo({
+    top: 0,
+    behavior: "smooth",
+  });
 }
