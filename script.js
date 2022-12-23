2760dfda6f0f9a073fd9ecac7d0ce0072f67ae10
Alexandre Achain (Weizu) <alexandre.achain@gmail.com>
2022-12-23 01:47:08 +0100

    Update script.js

--- script.js
+++ script.js
@@ -109,9 +109,7 @@ window.onload = (event) => {
 
   btnFav.addEventListener("click", () => {
     // Ajoute la page aux favoris
-    if (browser.bookmarks.create) {
-      var createBookmark = browser.bookmarks.create({title: currentTab.title, url: currentTab.url})
-    } else if (window.sidebar && window.sidebar.addPanel) {
+    if (window.sidebar && window.sidebar.addPanel) {
       // Pour les navigateurs anciens, tels que Firefox
       window.sidebar.addPanel(title, currentUrl, "");
     } else if (window.external && "AddFavorite" in window.external) {
